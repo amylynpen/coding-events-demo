af8e362702cf6449d671c936f2021c05fc3b87dd
Amy <amylynnroberts@gmail.com>
2021-10-14 20:24:15 -0500

    falling slowly

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,14 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jdk.jfr.Event;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.data.EventData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Chris Bay
